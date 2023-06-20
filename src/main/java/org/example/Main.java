8cfd1af89182b0372fbe5d67423e220cfee967f4
Melina <gamarramelina.2002@gmail.com>
2023-06-20 15:03:35 -0300

    esto es una prueba

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      //  Scanner numeros = new Scanner(System.in);
+      //  int a= 0, b=1, c, n;
+     //   System.out.println("ingrese un numero pue: ");
+       // n=numeros.nextInt();
+   //    for (int i =0; i<n; i++){
+      //     System.out.println(a+",");
+       //    c=a+b;
+          // a=b;
+         //  b=c;
+
+
+       //}
+
+        Scanner menu = new Scanner(System.in);
+        int opcion =0;
+        int m =0;
+        switch (opcion){
+            case 1:
+                System.out.println("ingrese a un modulo");
+                m = menu.nextInt();
+
+
+        }
+
+
+
+
+
+
+    }
+}
